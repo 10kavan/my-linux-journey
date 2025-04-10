acf522b0c9a67d01e175582e98b4941c43bcb285
10kavan <kavangunkaur@dcmail.ca>
2025-04-10 20:29:54 +0000

    Interactive Tutorial

---
+++ interactive_tutorial.sh
@@ -0,0 +1,34 @@
+[32m Welcome to the Linux Terminal Tutorial! [0m
+[32m Let's learn some basic commands. [0m
+
+[34m Step 1: Listing files in your current directory. [0m
+[34m Command: ls. [0m
+README.md
+interactive_tutorial.sh
+
+[36m Step 2: Making a new directory. [0m
+[36m Command: mkdir my_folder  [0m
+
+[20m Step 3: Navigating into your folder [0m
+[20m Command:cd my_folder [0m
+[20m You are now in: /home/ubuntu/linux-commands-cheatsheet [0m
+
+[30m Step 4: Creating a file [0m
+[30m Command: touch hello.txt [0m
+[30m File 'hello.txt' created! [0m
+
+[37m Step 5: Viewing your file list [0m
+
+
+README.md
+hello.txt
+interactive_tutorial.sh
+my_folder
+
+[31m You have just used: [0m
+[31m -ls [0m
+[31m -mkdir [0m
+[31m -cd [0m
+[31m -touch [0m
+
+[31m 🎉Congrats on completing the basics! [0m
